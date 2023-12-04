8bdb73d892486dbf34f9728ae9256ffc056a1cb0
Angel90226 <angel.chung@iss.nthu.edu.tw>
2023-12-04 15:54:51 +0800

    fixing rubocop

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -96,7 +96,7 @@ module FlyHii
             begin
               puts hashtag_name
               posts = Repository::For.klass(Entity::Post)
-                .find_full_name(hashtag_name)
+                .find_full_name
 
               if posts.nil?
                 flash[:error] = 'Hashtag not found'
